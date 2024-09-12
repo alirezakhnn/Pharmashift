1a77e2b256194e43089ffdd608f8ca84af938727
alirezakhnn <alirezakhnmohammad@gmail.com>
2024-09-13 00:18:05 +0330

    just started home page

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,10 +32,16 @@ export default function RootLayout({
         >
           <main className="min-h-screen flex flex-col items-center">
             <div className="flex-1 w-full flex flex-col gap-20 items-center">
-              <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
+              <nav className="w-full flex justify-center h-16">
                 <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
                   <div className="flex gap-5 items-center font-semibold">
                     <ThemeSwitcher />
+                    <div className="flex gap-3">
+                    فارماشیفت
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-capsule" viewBox="0 0 16 16">
+                      <path d="M1.828 8.9 8.9 1.827a4 4 0 1 1 5.657 5.657l-7.07 7.071A4 4 0 1 1 1.827 8.9Zm9.128.771 2.893-2.893a3 3 0 1 0-4.243-4.242L6.713 5.429z" />
+                    </svg>
+                    </div>
                   </div>
                   {!hasEnvVars ? <EnvVarWarning /> : <HeaderAuth />}
                 </div>
@@ -44,7 +50,7 @@ export default function RootLayout({
                 <GoogleCaptchaWrapper>{children}</GoogleCaptchaWrapper>
               </div>
 
-              <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
+              <footer className="w-full flex items-center justify-center mx-auto text-center text-xs gap-8 py-16">
                 <p>
                 </p>
               </footer>
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,21 @@
-import Hero from "@/components/hero";
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
 
 export default async function Index() {
   return (
     <>
-      <Hero />
-      <main className="flex-1 flex flex-col gap-6 px-4">
+      <main className="grid grid-cols-2 gap-x-6 z-[-10]">
+        <div>
+          <Image src="" alt="" fill />
+        </div>
+        <div className="grid gap-y-3 w-4/3">
+          <h1 className="text-[45px] font-bold">دستیار هوشمند تبادل شیفت داروسازان!</h1>
+          <p>به فارماشیفت، اولین سامانه تبادل شیفت مسئولین فنی داروساز، خوش آمدید.</p>
+          <div className="flex gap-4 mt-4">
+            <Button>ثبت نام داروسازان</Button>
+            <Button>متقاضیان تبادل شیفت</Button>
+          </div>
+        </div>
       </main>
     </>
   );
